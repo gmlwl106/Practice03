b427164d2abf6e3b42b74cd282f56f262c535c7c
gmlwl106 <66778894@naver.com>
2022-04-19 16:14:27 +0900

    Practice03. 숫자맞추기게임

--- src/com/javaex/practice03/Ex20.java
+++ src/com/javaex/practice03/Ex20.java
@@ -7,27 +7,34 @@ public class Ex20 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("==================================");
-		System.out.println("\t[숫자맞추기게임 시작]");
-		System.out.println("==================================");
-
-		int num = 55; //숫자 설정
+		Loop1 :
 		while(true) {
-			System.out.print(">>");
-			int myNum = sc.nextInt();
-			
-			if(myNum > num) {
-				System.out.println("더 낮게");
-			} else if(myNum < num) {
-				System.out.println("더 높게");
+			System.out.println("==================================");
+			System.out.println("\t[숫자맞추기게임 시작]");
+			System.out.println("==================================");
+	
+			int num = (int)(Math.random()*100)+1; //숫자 설정
+
+			Loop2 :
+			while(true) {
+				System.out.print(">>");
+				int myNum = sc.nextInt();
 				
-			} else if (myNum == num) {
-				System.out.println("맞았습니다.");
-				System.out.print("게임을 종료하시겠습니까?(y/n) >>");
-				char y_n = sc.next().charAt(0); //char형식 scanner
-				
-				if(y_n == 'y') {
-					break; //게임 종료
+				if(myNum > num) {
+					System.out.println("더 낮게");
+				} else if(myNum < num) {
+					System.out.println("더 높게");
+					
+				} else if (myNum == num) {
+					System.out.println("맞았습니다.");
+					System.out.print("게임을 종료하시겠습니까?(y/n) >>");
+					char y_n = sc.next().charAt(0); //char형식 scanner
+					
+					if(y_n == 'y') {
+						break Loop1; //게임 종료
+					} else {
+						break;
+					}
 				}
 			}
 		}
