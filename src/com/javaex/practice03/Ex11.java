dba0a8e3403cbcf5e3e7fa09677f39da0eaf78ac
gmlwl106 <66778894@naver.com>
2022-04-19 11:25:27 +0900

    practice03

--- src/com/javaex/practice03/Ex11.java
+++ src/com/javaex/practice03/Ex11.java
@@ -11,25 +11,21 @@ public class Ex11 {
 		int num = sc.nextInt(); //숫자 입력
 		int result = 0; //결과값 변수
 		
-		while(true) {
-			if(num%2 == 0) { //숫자가 짝수일때
-				for(int i=1; i<=num; i++) { //입력받은 숫자만큼 반복
-					if(i%2 == 0) { //짝수인 수를 합함
-						result += i;
-					} else {
-						continue;
-					}
+		if(num%2 == 0) { //숫자가 짝수일때
+			for(int i=1; i<=num; i++) { //입력받은 숫자만큼 반복
+				if(i%2 == 0) { //짝수인 수를 합함
+					result += i;
+				} else {
+					continue;
 				}
-				break; //입력된 숫자만큼 계산이 됐으면 반복 중단
-			} else { //숫자가 홀수일때
-				for(int i=1; i<=num; i++) { //입력받은 숫자만큼 반복
-					if(i%2 != 0) { //홀수인 수를 합함
-						result += i;
-					} else {
-						continue;
-					}
+			}
+		} else { //숫자가 홀수일때
+			for(int i=1; i<=num; i++) { //입력받은 숫자만큼 반복
+				if(i%2 != 0) { //홀수인 수를 합함
+					result += i;
+				} else {
+					continue;
 				}
-				break; //입력된 숫자만큼 계산이 됐으면 반복 중단
 			}
 		}
 		
